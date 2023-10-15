9b7bad878b4384fea623fe08eda8103a321c9b9e
Lucas Gimenes <lucas_sin5@hotmail.com>
2023-10-15 12:33:33 -0300

    setup database serverless neondb

--- api/src/lib/prisma.ts
+++ api/src/lib/prisma.ts
@@ -1,3 +1,18 @@
+// Import needed packages
+import { Pool, neonConfig } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from "@prisma/client";
+import dotenv from 'dotenv';
+import ws from 'ws';
 
-export const prisma = new PrismaClient()
+// Setup
+dotenv.config()
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.DATABASE_URL}`;
+
+// Init prisma client
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+export const prisma = new PrismaClient({ adapter });
+
+// Use Prisma Client as normal
